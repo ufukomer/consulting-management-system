2cd08fd85b26d28ef7dd29912655ee643c2e7340
melihkerman <muhammedmelihkerman@gmail.com>
2015-05-22 16:14:48 +0300

    personelBean

--- src/main/java/com/mycompany/consulting/management/system/bean/PersonnelBean.java
+++ src/main/java/com/mycompany/consulting/management/system/bean/PersonnelBean.java
@@ -11,8 +11,81 @@ package com.mycompany.consulting.management.system.bean;
  * @author MelihKerman
  */
 public class PersonnelBean {
-    int id;
-    String name;
-    String surname;
+   
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String role;
+    private int salary;
+    private boolean isWorking;
+            
     
+    public PersonnelBean(){
+        
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public boolean isIsWorking() {
+        return isWorking;
+    }
+
+    public void setIsWorking(boolean isWorking) {
+        this.isWorking = isWorking;
+    }
 }

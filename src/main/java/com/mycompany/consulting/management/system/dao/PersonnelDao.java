815699d589a59fe9966b410615fcc3a13b715cfb
unknown <ufukomer@hotmail.com>
2015-05-22 16:44:07 +0300

    personneldao&mysqlconnector

--- src/main/java/com/mycompany/consulting/management/system/dao/PersonnelDao.java
+++ src/main/java/com/mycompany/consulting/management/system/dao/PersonnelDao.java
@@ -3,13 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.mycompany.consulting.management.system.dao;
+
+import com.mycompany.consulting.management.system.bean.PersonnelBean;
+import java.util.List;
 
 /**
  *
  * @author MelihKerman
  */
 public interface PersonnelDao {
+
+    boolean addPersonnel(PersonnelBean personnelBean);
+
+    PersonnelBean getPersonnelById(int id);
+
+    PersonnelBean getPersonelByEmail(String email);
+
+    boolean updatePersonnelById(int id);
+
+    boolean deletePersonnelById(int id);
     
+    List<PersonnelBean> getAllPersonnel();
 }

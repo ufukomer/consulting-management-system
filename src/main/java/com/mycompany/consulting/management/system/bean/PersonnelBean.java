3fba23139aeef6988371b3a7692936d98aabe95f
melihkerman <muhammedmelihkerman@gmail.com>
2015-05-22 16:10:42 +0300

    ses kontrol2

---
+++ src/main/java/com/mycompany/consulting/management/system/bean/PersonnelBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.consulting.management.system.bean;
+
+/**
+ *
+ * @author MelihKerman
+ */
+public class PersonnelBean {
+    int id;
+    String name;
+    String surname;
+    
+}
